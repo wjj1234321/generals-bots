b26268d766587684e218119a89debcfabee00c14
wjj1234321 <wujiaju4@gmail.com>
2025-06-05 02:36:08 +0800

    能运行成功的案例

--- examples/complete_example.py
+++ examples/complete_example.py
@@ -1,17 +1,20 @@
 from generals import GridFactory, PettingZooGenerals
 from generals.agents import RandomAgent, ExpanderAgent
+import time
 
 agent = ExpanderAgent()
-npc = RandomAgent()
+agent.id = 'smart1'
+
+npc = ExpanderAgent()
+npc.id = 'smart2'
 
 # Initialize grid factory
 grid_factory = GridFactory(
-    mode="uniform",  # alternatively "generalsio", which will override other parameters
-    min_grid_dims=(15, 15),  # Grid height and width are randomly selected
-    max_grid_dims=(23, 23),
+     # alternatively "generalsio", which will override other parameters
+    grid_dims=(15, 15),
     mountain_density=0.2,  # Expected percentage of mountains
     city_density=0.05,  # Expected percentage of cities
-    seed=38,  # Seed to generate the same map every time
+    seed = 121,  # Seed to generate the same map every time
 )
 
 agents = {
@@ -19,7 +22,10 @@ agents = {
     npc.id: npc,
 }
 
-env = PettingZooGenerals(agents=[agent.id, npc.id], grid_factory=grid_factory)
+
+
+env = PettingZooGenerals(agents=agents, grid_factory=grid_factory, render_mode="human")
+
 
 # We can draw custom maps - see symbol explanations in README
 grid = """
@@ -41,12 +47,23 @@ options = {
 }
 
 observations, info = env.reset(options=options)
-terminated = truncated = False
-while not (terminated or truncated):
+terminated = truncated = {}
+step_count = 0
+while not any(terminated.values()):
     actions = {}
-    for agent in env.agents:
-        # Ask agent for action
-        actions[agent] = agents[agent].act(observations[agent])
-    # All agents perform their actions
-    observations, rewards, terminated, truncated, info = env.step(actions)
-    env.render()
+    for agent in env.agents: #每轮所有玩家同时决定一次操作
+        action = agents[agent].act(observations[agent]) #告诉玩家视野
+        # 检查 agent 行为是否合法
+        if action is None:
+            raise ValueError(f"Agent {agent} 在第 {step_count} 步返回了 None 动作，可能实现有误。")
+        actions[agent] = action #记录所有玩家的移动
+    print(f"Step {step_count}, actions: {actions}")
+    observations, rewards, terminated, truncated, info = env.step(actions) #执行移动，并输出信息
+    print(f"rewards: {rewards}, terminated: {terminated}, truncated: {truncated}")
+    env.render() #更新画面
+    step_count += 1
+
+
+
+print("游戏结束，窗口将在5秒后关闭...")
+time.sleep(5)
